e0a5e82bf2614a8854ceef1e592eeb57559d78ab
Csige Zoltán <zoli0198@gmail.com>
2024-03-20 21:32:22 +0100

    Add files via upload

---
+++ your-firebase-config.js
@@ -0,0 +1,34 @@
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+// your-firebase-config.js
+var firebaseConfig = {
+	apiKey: "AIzaSyCBxCkbfDppCYFoGpYgHQd25V6eYMvApVc",
+	authDomain: "idproject-7a269.firebaseapp.com",
+	databaseURL: "https://idproject-7a269-default-rtdb.europe-west1.firebasedatabase.app",
+	projectId: "idproject-7a269",
+	storageBucket: "idproject-7a269.appspot.com",
+	messagingSenderId: "432108778658",
+	appId: "1:432108778658:web:7243d3131e301cd7d413fb",
+	measurementId: "G-N7KQJQJP7T"
+};
+
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+
+
